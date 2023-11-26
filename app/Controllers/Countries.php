778856a0085e36f7bf322b56a5b57c869e4291f7
Rebeca T <rebecatucmeanu@Rebecas-MacBook-Air.local>
2023-11-26 15:11:43 +0100

    modificaciones sin guardar

--- app/Controllers/Countries.php
+++ app/Controllers/Countries.php
@@ -9,4 +9,9 @@ class Countries extends Controller
     public function index() {
         return view('templates/header').view('templates/index').view('templates/footer');
     }
+
+    public function showCountry($country) { 
+        $data ['country'] = $country;
+        return view('templates/header', $data).view('templates/index').view('templates/footer');
+    }
 }
