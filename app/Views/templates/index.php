ed6a7f68248953f72df51c95327a8302755e4d70
Rebeca T <rebecatucmeanu@Rebecas-MacBook-Air.local>
2023-11-27 10:46:29 +0100

    resolución problemas

--- app/Views/templates/index.php
+++ app/Views/templates/index.php
@@ -5,23 +5,16 @@
             <h4>Countries</h4>
         </div>
         <ul class="list-unstyled">
-            <li><a href="#" onclick="showCountry('Spain')">Spain</a></li>
-            <li><a href="#" onclick="showCountry('France')">France</a></li>
-            <li><a href="#" onclick="showCountry('Germany')">Germany</a></li>
-            <li><a href="#" onclick="showCountry('Italy')">Italy</a></li>
-            <li><a href="#" onclick="showCountry('United Kingdom')">United Kingdom</a></li>
-            <li><a href="#" onclick="showCountry('Portugal')">Portugal</a></li>
-            <li><a href="#" onclick="showCountry('Netherlands')">Netherlands</a></li>
-            <li><a href="#" onclick="showCountry('Sweden')">Sweden</a></li>
-            <li><a href="#" onclick="showCountry('Switzerland')">Switzerland</a></li>
-            <li><a href="#" onclick="showCountry('Norway')">Norway</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Home">Home</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Spain">Spain</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/France">France</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Germany">Germany</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Italy">Italy</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/UnitedKingdom">United Kingdom</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Portugal">Portugal</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Netherlands">Netherlands</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Sweden">Sweden</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Switzerland">Switzerland</a></li>
+            <li><a href="<?php echo base_url()?>Countries/showCountry/Norway">Norway</a></li>
         </ul>
     </nav>
-    <div id="content">
-        <h2>Information</h2>
-        <div id="country-info">
-            <h1>Select a country</h1>
-            <p>Select a country from the sidebar to view information.</p>
-        </div>
-    </div>
-</div>
