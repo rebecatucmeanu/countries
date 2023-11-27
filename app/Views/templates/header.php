bc1f9a60e0e817354143219cf42a18dfdc2c2afd
Rebeca T <rebecatucmeanu@Rebecas-MacBook-Air.local>
2023-11-27 10:45:14 +0100

    correcciones header

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,5 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Información de Países</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>styles.css">
 </head>
