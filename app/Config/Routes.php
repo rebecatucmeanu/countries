3bb15296225d03a9296ba7c0fdadd3e53248114f
Rebeca T <rebecatucmeanu@Rebecas-MacBook-Air.local>
2023-11-27 12:11:57 +0100

    modificar ruta principal

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Countries::index');
+$routes->get('/', 'Countries::index/Home');
 // $routes->get('/mostrar', 'Home::mostrar_usuario');
